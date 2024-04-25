06f2096fc41e1063a69a73e6e43a58e58085fe2c
Noodle98 <46034007+Noodle98@users.noreply.github.com>
2024-04-25 12:11:44 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+    // data, functions
+   data() {
+    return {
+        showBooks: true,
+        title: 'The Final Empire',
+        author: 'Brandon Sanderson',
+        age: 45
+    }
+   },
+   methods: {
+    toggleShowBooks() {
+        this.showBooks = !this.showBooks
+    },
+    handleEvent() {
+        console.log('event')
+    }
+   }
+})
+
+app.mount('#app')
